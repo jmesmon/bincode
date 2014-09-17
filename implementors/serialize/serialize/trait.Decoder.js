911f568fb497631aee631e9d5fa1fce1fa9adfb9
Ty Overby <ty@pre-alpha.com>
2014-09-17 02:58:58 -0700

    updated to 8c0ec56

---
+++ implementors/serialize/serialize/trait.Decoder.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['binary_encode'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;R: <a class='trait' href='http://doc.rust-lang.org/master/std/io/trait.Reader.html' title='std::io::Reader'>Reader</a>&gt; <a class='trait' href='http://doc.rust-lang.org/master/serialize/serialize/trait.Decoder.html' title='serialize::serialize::Decoder'>Decoder</a>&lt;<a class='struct' href='http://doc.rust-lang.org/master/std/io/struct.IoError.html' title='std::io::IoError'>IoError</a>&gt; for <a class='struct' href='binary_encode/struct.DecoderReader.html' title='binary_encode::DecoderReader'>DecoderReader</a>&lt;R&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
