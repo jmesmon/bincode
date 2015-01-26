6cfd662b59ca4146d1c40c2c33c9c5925f3553ea
Ty Overby <ty@pre-alpha.com>
2015-01-26 09:35:12 -0800

    update docs to ced9a95

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bincode'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='bincode/enum.DecodingError.html' title='bincode::DecodingError'>DecodingError</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='bincode/enum.SizeLimit.html' title='bincode::SizeLimit'>SizeLimit</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/rustc-serialize/serialize/trait.Encoder.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bincode'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, W: <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Writer.html' title='std::io::Writer'>Writer</a>&gt; <a class='trait' href='rustc-serialize/serialize/trait.Encoder.html' title='rustc-serialize::serialize::Encoder'>Encoder</a> for <a class='struct' href='bincode/struct.EncoderWriter.html' title='bincode::EncoderWriter'>EncoderWriter</a>&lt;'a, W&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
