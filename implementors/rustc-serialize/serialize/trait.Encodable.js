3dd0baa2fa15b604d131782a46ad4449e7099a42
Ty Overby <ty@pre-alpha.com>
2015-02-18 14:32:22 -0800

    update docs

---
+++ implementors/rustc-serialize/serialize/trait.Encodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['bincode'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a, T: <a class='trait' href='rustc-serialize/serialize/trait.Encodable.html' title='rustc-serialize::serialize::Encodable'>Encodable</a>&gt; <a class='trait' href='rustc-serialize/serialize/trait.Encodable.html' title='rustc-serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='bincode/struct.RefBox.html' title='bincode::RefBox'>RefBox</a>&lt;'a, T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
